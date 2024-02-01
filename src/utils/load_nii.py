a5d65f2381b05e585dd9b135830d1c2c896bcb98
NatLee <natlee.work@gmail.com>
2024-02-01 17:56:51 +0800

    [fix] use new method

--- src/utils/load_nii.py
+++ src/utils/load_nii.py
@@ -1,5 +1,6 @@
+import numpy as np
 import nibabel as nib
 
 def load_nii(path:str):
     nii = nib.load(path)
-    return nii.get_data(), nii.affine
+    return np.asanyarray(nii.dataobj), nii.affine
