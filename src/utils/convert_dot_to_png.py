67c289b7a01851c0400f12f7adae982cf8c9bc27
NatLee <natlee.work@gmail.com>
2024-02-01 23:36:28 +0800

    [fix] blank rectangle

--- src/utils/convert_dot_to_png.py
+++ src/utils/convert_dot_to_png.py
@@ -10,10 +10,14 @@ def convert_dot_to_png(dot_file_path, png_file_path):
     dot_file_path (str): The path to the DOT file.
     png_file_path (str): The path to save the PNG file.
     """
-    # Load the dot file
-    graphs = pydot.graph_from_dot_file(dot_file_path)
+    # Load the dot file from string
+    with open(dot_file_path) as f:
+        dot_file_contents = f.read()
+        dot_file_contents = dot_file_contents.replace('\n', '') # avoid blank rectangle in the png file
+
+    (graph,) = pydot.graph_from_dot_data(dot_file_contents)
 
     # Save the dot file as a PNG file
-    graphs[0].write_png(png_file_path)
+    graph.write_png(png_file_path)
 
     logger.info(f"Saved PNG file to {png_file_path}")
