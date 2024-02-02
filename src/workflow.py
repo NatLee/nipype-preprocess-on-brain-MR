cdb6ee31f7c2991bcc5d6c12f1c75d266483ebc8
NatLee <natlee.work@gmail.com>
2024-02-02 17:12:35 +0800

    [update] add pair node for processing input list

--- src/workflow.py
+++ src/workflow.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import shutil
 
+from nipype import Function
 from nipype.pipeline import Node, Workflow
-from nipype.interfaces.utility import IdentityInterface
 
 from node.acpc_detect.interface import ACPCDetectInterface
 from node.zip.interface import ZipOutputInterface
@@ -19,68 +19,83 @@ from utils.convert_dot_to_png import convert_dot_to_png
 # ==========================================
 
 # Define the input file and output folder
-input_file = Path('/data/099_S_4205.nii')
-output_folder = Path('/data/099_S_4205')
+# List of input NIfTI files to process
+input_files = [
+    Path('/data/099_S_4205.nii'),
+    Path('/data/099_S_4205_2.nii')
+]
 
-# Clean up the output folder if it already exists
-if output_folder.exists():
-    shutil.rmtree(output_folder)
-output_folder.mkdir(parents=True, exist_ok=True)
-output_folder.chmod(0o777)
+output_folder = Path('/output')
 
-# Define input node with 'input_file' as its input field
-input_file_node = Node(IdentityInterface(fields=['input_file']), name='inputspec')
-input_file_node.inputs.input_file = input_file
+output_folders = []
+for input_file in input_files:
+    folder = output_folder / input_file.stem
+    folder.mkdir(parents=True, exist_ok=True)
+    output_folders.append(folder)
+    # Clean the folder
+    for file in folder.glob('*'):
+        if file.is_file():
+            file.unlink()
+        if file.is_dir():
+            shutil.rmtree(file)
+
+pairs = list(zip(input_files, output_folders))
+
+# ==========================================
+
+# Define the process_pair function
+def process_pair(pair):
+    input_file, output_folder = pair
+    return input_file, output_folder
+
+process_pair_node = Node(
+    Function(
+        input_names=["pair"],
+        output_names=["input_file", "output_folder"],
+        function=process_pair
+    ),
+    name="process_pair"
+)
+process_pair_node.iterables = [('pair', pairs)]
 
 # Create the acpc node
 acpc_node = Node(ACPCDetectInterface(), name='t1_acpc_detect')
-acpc_node.inputs.output_folder = output_folder
 
 # Create the zip node
 zip_node = Node(ZipOutputInterface(), name='zip_output')
-zip_node.inputs.output_folder = output_folder
 
 # Create the orient2std node
 orient2std_node = Node(Reorient2StdInterface(), name='orient2std')
-orient2std_node.inputs.output_folder = output_folder
 
 # Create the registration node
 registration_node = Node(FLIRTInterface(), name='registration')
-registration_node.inputs.output_folder = output_folder
 
 # Create the skull stripping node
 skull_stripping_node = Node(SkullStrippingInterface(), name='skull_stripping')
-skull_stripping_node.inputs.output_folder = output_folder
 
 # Create the bias field correction node
 bias_field_correction_node = Node(BiasFieldCorrectionInterface(), name='bias_field_correction')
-bias_field_correction_node.inputs.output_folder = output_folder
 
 # Create the enhancement node
 enhancement_node = Node(EnhancementInterface(), name='enhancement')
-enhancement_node.inputs.output_folder = output_folder
 
 # Create the segmentation node
 segmentation_node = Node(SegmentationInterface(), name='segmentation')
-segmentation_node.inputs.output_folder = output_folder
 
 # Create the draw segmentation node
 draw_gm_segmentation_node = Node(DrawSegmentationInterface(), name='draw_gm_segmentation')
-draw_gm_segmentation_node.inputs.output_folder = output_folder
-draw_gm_segmentation_node.inputs.title = 'GM Prob. map'
+draw_gm_segmentation_node.inputs.title = 'GM@map'
 draw_wm_segmentation_node = Node(DrawSegmentationInterface(), name='draw_wm_segmentation')
-draw_wm_segmentation_node.inputs.output_folder = output_folder
-draw_wm_segmentation_node.inputs.title = 'WM Prob. map'
+draw_wm_segmentation_node.inputs.title = 'WM@map'
 draw_csf_segmentation_node = Node(DrawSegmentationInterface(), name='draw_csf_segmentation')
-draw_csf_segmentation_node.inputs.output_folder = output_folder
-draw_csf_segmentation_node.inputs.title = 'CSF Prob. map'
+draw_csf_segmentation_node.inputs.title = 'CSF@map'
 
 # ==========================================
 
 # Create a workflow
 workflow = Workflow(name='preprocess_workflow')
 nodes = [
-    input_file_node,
+    process_pair_node,
     acpc_node,
     zip_node,
     orient2std_node,
@@ -95,41 +110,50 @@ nodes = [
 ]
 workflow.add_nodes(nodes)
 
-# Connect the input_file_node to the acpc_node
-workflow.connect(input_file_node, 'input_file', acpc_node, 'input_file')
+# Connect the input_file_node to the acpc_node (input_file & output_folder)
+workflow.connect(process_pair_node, 'input_file', acpc_node, 'input_file')
+workflow.connect(process_pair_node, 'output_folder', acpc_node, 'output_folder')
 
-# Connect the acpc_node to the zip_node
+# Connect the acpc_node to the zip_node (input_file & output_folder)
 workflow.connect(acpc_node, 'output_file', zip_node, 'input_file')
+workflow.connect(process_pair_node, 'output_folder', zip_node, 'output_folder')
 
-# Connect the zip_node to the orient2std_node
+# Connect the zip_node to the orient2std_node (input_file & output_folder)
 workflow.connect(zip_node, 'output_file', orient2std_node, 'input_file')
+workflow.connect(process_pair_node, 'output_folder', orient2std_node, 'output_folder')
 
-# Connect the orient2std_node to the registration_node
+# Connect the orient2std_node to the registration_node (input_file & output_folder)
 workflow.connect(orient2std_node, 'output_file', registration_node, 'input_file')
+workflow.connect(process_pair_node, 'output_folder', registration_node, 'output_folder')
 
-# Connect the registration_node to the skull_stripping_node
+# Connect the registration_node to the skull_stripping_node (input_file & output_folder)
 workflow.connect(registration_node, 'output_file', skull_stripping_node, 'input_file')
+workflow.connect(process_pair_node, 'output_folder', skull_stripping_node, 'output_folder')
 
-# Connect the skull_stripping_node to the bias_field_correction_node
+# Connect the skull_stripping_node to the bias_field_correction_node (input_file & output_folder)
 workflow.connect(skull_stripping_node, 'output_file', bias_field_correction_node, 'input_file')
+workflow.connect(process_pair_node, 'output_folder', bias_field_correction_node, 'output_folder')
 
-# Connect the bias_field_correction_node to the enhancement_node
+# Connect the bias_field_correction_node to the enhancement_node (input_file & output_folder)
 workflow.connect(bias_field_correction_node, 'output_file', enhancement_node, 'input_file')
+workflow.connect(process_pair_node, 'output_folder', enhancement_node, 'output_folder')
 
-# Connect the enhancement_node to the segmentation_node
+# Connect the enhancement_node to the segmentation_node (input_file & output_folder)
 workflow.connect(enhancement_node, 'output_file', segmentation_node, 'input_file')
+workflow.connect(process_pair_node, 'output_folder', segmentation_node, 'output_folder')
 
-
-# Connect acpc_node to draw_gm_segmentation_node
+# Connect acpc_node to draw_gm_segmentation_node (input_file & output_folder)
 workflow.connect(acpc_node, 'output_file', draw_gm_segmentation_node, 'acpc_input_file')
 workflow.connect(segmentation_node, 'gm_segmented_output_file', draw_gm_segmentation_node, 'segmented_input_file')
-# Connect acpc_node to draw_wm_segmentation_node
+workflow.connect(process_pair_node, 'output_folder', draw_gm_segmentation_node, 'output_folder')
+# Connect acpc_node to draw_wm_segmentation_node (input_file & output_folder)
 workflow.connect(acpc_node, 'output_file', draw_wm_segmentation_node, 'acpc_input_file')
 workflow.connect(segmentation_node, 'wm_segmented_output_file', draw_wm_segmentation_node, 'segmented_input_file')
-# Connect acpc_node to draw_csf_segmentation_node
+workflow.connect(process_pair_node, 'output_folder', draw_wm_segmentation_node, 'output_folder')
+# Connect acpc_node to draw_csf_segmentation_node (input_file & output_folder)
 workflow.connect(acpc_node, 'output_file', draw_csf_segmentation_node, 'acpc_input_file')
 workflow.connect(segmentation_node, 'csf_segmented_output_file', draw_csf_segmentation_node, 'segmented_input_file')
-
+workflow.connect(process_pair_node, 'output_folder', draw_csf_segmentation_node, 'output_folder')
 
 # ==========================================
 
